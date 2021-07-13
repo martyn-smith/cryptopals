42c69277c9661be1643df1a3ed9495f13b97c99e
Martyn <martyn.smith560@outlook.com>
2021-07-13 22:42:04 +0100

    moved from pycrypto to pycryptodome

--- set2/c16.py
+++ set2/c16.py
@@ -6,7 +6,9 @@ CBC bitflipping attacks (from https://cryptopals.com/sets/2/challenges/16)
 from Crypto.Cipher import AES
 from utils import BLOCK_SIZE, generate_key, generate_IV, pad
 
-a = AES.new(generate_key(), AES.MODE_CBC, generate_IV())
+key = generate_key()
+IV = generate_IV()
+a, b = AES.new(key, AES.MODE_CBC, IV), AES.new(key, AES.MODE_CBC, IV)
 
 def surrounded_CBC(msg):
     prefix = "comment1=cooking%20MCs;userdata="
@@ -14,15 +16,12 @@ def surrounded_CBC(msg):
     msg = prefix.replace(";","").replace("=","") + msg.replace(";","").replace("=","") + postfix.replace(";","").replace("=","")
     return a.encrypt(pad(bytes(msg, encoding="utf-8")))
 
-def find_admin_string(msg):
-    return "admin=true" in msg
-
 def bitflip_cbc():
     ciphertext = surrounded_CBC("   " + "admin" + chr(0) + "true")
-    target = a.decrypt(ciphertext).index(b"admin") + 5 - BLOCK_SIZE
+    target = b.decrypt(ciphertext).index(b"admin") + 5 - BLOCK_SIZE
     ciphertext = ciphertext[0:target] + bytes([ciphertext[target] ^ ord("=")]) + ciphertext[target+1:]
-    new_plaintxt = "".join([chr(i) for i in a.decrypt(ciphertext)])
-    return find_admin_string(new_plaintxt)
+    new_plaintxt = "".join([chr(i) for i in b.decrypt(ciphertext)])
+    return "admin=true" in new_plaintxt
 
 if __name__ == "__main__":
     print(bitflip_cbc())
