aed725f4ec1ded5527c94a137a354e9f6cffb1ae
Martyn <martyn.smith560@outlook.com>
2021-07-21 20:30:15 +0100

    mersenne bugfix

--- set3/c21.py
+++ set3/c21.py
@@ -2,8 +2,13 @@
 Set 3 Challenge 21: Implement the Mersenne Twister (https://cryptopals.com/sets/3/challenges/21)
 
 """
+"""
+Twister logic implementation courtesy of James27 (https://github.com/james727)
+
 #TODO: this still doesn't match irb's random() beyond the first two, but literally no two Mersenne
 #generators seem to actually match, so...
+"""
+
 def MT19937(seed):
     """
     The series x is defined as a series of w-bit quantities with the recurrence relation:
@@ -27,12 +32,12 @@ def MT19937(seed):
     m = 397
     a = 0x9908b0df
 
-    #TODO: upper should be upper (w-r = 1) bits, lower should be lower (r=31) bits
+    #temper(42) = 1521226794
     upper = lambda x : x >> r
     lower = lambda x : x & (1 << (r+1)) - 1
-    lowest = lambda x: x % 2
-    #A = lambda x : x = x >> 1; x if lowest(x) == 0 else x ^ a
+    A = lambda x : x >> 1 if x % 2 == 0 else (x >> 1) ^ a
 
+    #this appears to work fine
     def init(seed):
         f = 1812433253
         x = [seed]
@@ -42,18 +47,12 @@ def MT19937(seed):
         return x
 
     def twist(x):
-        """
-        Twister logic implementation courtesy of James27 (https://github.com/james727)
-        """
         wrap_32 = lambda x: x & 0xffffffff
         lower_mask = (1<<r)-1
         upper_mask = 1<<r
         for i in range(n):
             temp = wrap_32((x[i] & upper_mask)+(x[(i+1) % n] & lower_mask))
-            temp_shift = temp >> 1
-            if temp_shift % 2 != 0:
-                temp_shift ^= a
-            x[i] = x[(i+m) % n]^temp_shift
+            x[i] = x[(i+m) % n]^ A(temp) 
         return x
 
     def dtwist(x):
@@ -78,13 +77,11 @@ def MT19937(seed):
 
     x = init(seed)
     x = twist(x)
-    
     i = 0
     while True:
         if i == n:
             x = twist(x)
             i = 0
-        #temper(42) = 1521226794
         yield temper(x[i]) #temper(x[i])
         i += 1
 
